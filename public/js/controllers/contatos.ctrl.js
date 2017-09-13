9b683dfdf18a2c1b88b48d735d257453339ebfbd
Adriano Reis <adriano_silvareis@hotmail.com>
2017-09-13 20:12:22 -0300

    Aula 4

--- public/js/controllers/contatos.ctrl.js
+++ public/js/controllers/contatos.ctrl.js
@@ -2,5 +2,13 @@ angular.module('agenda')
   .controller('contatosCtrl', ["$scope", contatoCtrl])
 
   function contatoCtrl($scope){
-    $scope.title = "teste"
+    $scope.title = "Contatos"
+
+    $scope.contato = {}
+    $scope.contatos = []
+
+    $scope.adicionar = (contato) => {
+      $scope.contatos.push(angular.copy(contato))
+      delete $scope.contato
+    }
   }
