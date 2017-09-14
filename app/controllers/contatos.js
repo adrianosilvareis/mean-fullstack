c357c3e56f4b795519ae4726339408e5b0048e98
Adriano Reis <adriano_silvareis@hotmail.com>
2017-09-14 15:35:59 -0300

    Aula 9

--- app/controllers/contatos.js
+++ app/controllers/contatos.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
   let controller = {}
 
+  // simula objeto vindo do banco
   let contatos = [
     {_id: 1, nome: 'contato 1', email: 'cont1@email.com'},
     {_id: 2, nome: 'contato 2', email: 'cont2@email.com'},
@@ -9,19 +10,35 @@ module.exports = (app) => {
     {_id: 5, nome: 'contato 5', email: 'cont5@email.com'},
     {_id: 6, nome: 'contato 6', email: 'cont6@email.com'}
   ]
-  
+
+  // simula autoincremento do id
+  let _id = 6
+
+  // filtra os dados recebidos para garantir a segurança do sistema
+  let filtrarDados = contato => {
+    let contatoFiltrado = {
+      _id: contato._id,
+      nome: contato.nome,
+      email: contato.email
+    }
+
+    return contatoFiltrado
+  }
+
   controller.listarContatos = (req, res, next) => {
-      res.json(contatos)
+    res.json(contatos)
   }
 
   controller.obterContato = (req, res, next) => {
     let idContato = req.params.id
-    let contato = contatos.filter(contato => contato.id == idContato)[0]
+    let contato = contatos.filter(contato => contato._id == idContato)[0]
 
-    contato ? res.json(contato) : res.status(404).send("Contato não encontrado")
+    contato
+      ? res.json(contato)
+      : res.status(404).send("Contato não encontrado")
   }
 
-  controller.removerContato = (req, res, next) =>{
+  controller.removerContato = (req, res, next) => {
     let idContato = req.params.id
 
     contatos = contatos.filter(contato => idContato != contato._id)
@@ -29,10 +46,29 @@ module.exports = (app) => {
     res.end()
   }
 
-  controller.salvarContato = (req, res, next) =>{
-    console.log(req.body)
+  controller.salvarContato = (req, res, next) => {
+    let contato = req.body
 
-    res.send('OK')
+    contato._id
+      ? atualizar(contato)
+      : adicionar(contato)
+
+    res.json(contato)
+  }
+
+  let atualizar = (contatoAlterar) => {
+    // arrow function: (obj) => {função}  || operador ternário  ( condição ? true : false )
+    contatos = contatos.map( contato => (contato._id == contatoAlterar._id ? contatoAlterar : contato))
+  }
+
+  let adicionar = (contatoNovo) => {
+    contatoNovo._id = ++_id
+
+    contatoNovo = filtrarDados(contatoNovo)
+    
+    contatos.push(contatoNovo)
+
+    return contatoNovo
   }
 
   return controller
--- public/js/controllers/contato.ctrl.js
+++ public/js/controllers/contato.ctrl.js
@@ -3,17 +3,42 @@ angular.module('agenda')
 
   function contatoCtrl($scope, $routeParams, $http){
 
+    // salvar alteração ou novo contato
     $scope.salvar = (contato) => {
       $http.post('/contatos', contato)
         .then(success, error)
+
+      delete $scope.contato.message
     }
 
+    // função de successo
     function success(success){
-      console.log(success)
+      $scope.contato = success.data
     }
 
+    // função de retorno de erro
     function error(error){
       console.log("Error", error)
+      $scope.contato = { message: error.data }
     }
-    
+
+    // verifica se existe parametro
+    let init = (_id) => {
+      _id ?
+        carregaContato(_id) :
+        novoContato()
+    }
+
+    // se existir parametro carrega
+    function carregaContato(_id) {
+      $http.get("/contatos/" + _id)
+        .then(success, error)
+    }
+
+    // caso não exista inicia o objeto fazio
+    function novoContato(){
+      $scope.contato = {message: "Novo contato"}
+    }
+
+    init($routeParams.id)
   }
